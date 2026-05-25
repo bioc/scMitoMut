b32b9b661e609709d5664c4f593ea52d87ceca19
Wenjie Sun <sunwjie@gmail.com>
2026-05-25 01:28:27 -0400

    Declare NSE globals for check

---
+++ R/globals.R
@@ -0,0 +1,18 @@
+if (getRversion() >= "2.15.1") {
+    utils::globalVariables(c(
+        "af",
+        "alt",
+        "alt_count",
+        "alt_depth",
+        "cell_barcode",
+        "coverage",
+        "depth",
+        "fwd_depth",
+        "highlight",
+        "loc",
+        "mut_cell_n",
+        "mut_status",
+        "n",
+        "rev_depth"
+    ))
+}
